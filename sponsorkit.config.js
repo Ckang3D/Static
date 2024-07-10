3cf410c3cbeb0ff618fbe2d1429828c1d03daae2
Loudomian <ckang2021@gmail.com>
2024-07-10 16:25:00 +0800

    first commit

---
+++ sponsorkit.config.js
@@ -0,0 +1,71 @@
+import { defineConfig, tierPresets } from 'sponsorkit'
+
+export default defineConfig({
+
+  // includePrivate: true,
+  tiers: [
+    {
+      title: '💕 Past Sponsors',
+      monthlyDollars: -1,
+      preset: tierPresets.xs,
+    },
+    {
+      title: '🍨 Ice cream',
+      preset: tierPresets.base,
+    },
+    {
+      title: '🍵 Tea',
+      monthlyDollars: 5,
+      preset: tierPresets.medium,
+    },
+    {
+      title: '🍹 Tropical Drink',
+      monthlyDollars: 12,
+      preset: tierPresets.large,
+    },
+    {
+      title: '🍱 Bento',
+      monthlyDollars: 20,
+      preset: tierPresets.xl,
+    },
+  ],
+
+  // Replace links and avatars
+  // replaceLinks: {
+  //   'https://github.com/antfu': 'https://antfu.me',
+  // },
+  // replaceAvatars: {
+  //   ...
+  // },
+
+  // Automatically Merge sponsors from different platforms
+  sponsorsAutoMerge: true,
+
+  // Manually merge sponsors from different platforms
+  // mergeSponsors: [
+  //   [
+  //     { login: 'patak-dev', provider: 'github' },
+  //     { login: 'patak', provider: 'opencollective' },
+  //   ],
+  // ],
+
+  // Run multiple renders with different configurations
+  renders: [
+    {
+      name: 'sponsors',
+      width: 800,
+      formats: ['svg', 'png'],
+    },
+    {
+      name: 'sponsors-wide',
+      width: 1000,
+      formats: ['svg'],
+    },
+    {
+      renderer: 'circles',
+      name: 'sponsors-circles',
+      width: 1000,
+      includePastSponsors: true,
+    },
+  ],
+})
